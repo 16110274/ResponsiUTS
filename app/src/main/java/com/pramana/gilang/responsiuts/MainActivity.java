9194eea94d76231a7bec02bf4475e8029c606c9b
Gilang Adhitya <gilang.pramana@students.amikom.ac.id>
2018-10-28 16:18:07 +0700

    Initial commit

---
+++ app/src/main/java/com/pramana/gilang/responsiuts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pramana.gilang.responsiuts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
